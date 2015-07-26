1777da65328f1795ff1fc537f1e206becb9dd4d9
marshallbu <marshall@mbucreations.com>
2015-07-25 23:00:37 -0700

    modernize this plugin
    - get rid of react-tools and use babel
    - update lodash references to latest lodash
    - use eslint instead of jshint
    - update all dependencies
    - make editorconfig use 4 spaces
    - remove deprecated @jsx pragma
    - javascript the good parts
    - make tests pass in this new world
    - ignore a few things in eslint for now

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-/*
- * gulp-render
+/**
+ * gulp-render-react
  * https://github.com/koistya/gulp-render
  *
  * Copyright (c) 2014 Konstantin Tarkus
  * Licensed under the MIT license
  */
+
+/*eslint-disable max-statements, no-underscore-dangle, max-depth */
 
 'use strict';
 
@@ -12,38 +14,30 @@ var through = require('through2');
 var gutil = require('gulp-util');
 var path = require('path');
 var fs = require('fs');
-var _ = require('lodash');
 var React = require('react');
-var ReactTools = require('react-tools');
+var babel = require('babel');
 var hyphenate = require('react/lib/hyphenate');
-var template = _.template;
+var template = require('lodash/string/template');
+var extend = require('lodash/object/extend');
 var PluginError = gutil.PluginError;
 var Module = module.constructor;
 
 // Constants
-var PLUGIN_NAME = 'gulp-render';
-
-/**
- * Append @jsx pragma to JSX files
- */
-function appendJsxPragma(filename, contents) {
-  return filename.match(/\.jsx$/) || filename.match(/[\-\.]react\.js$/) ?
-  '/**@jsx React.DOM*/' + contents : contents;
-}
+var PLUGIN_NAME = 'gulp-render-react';
 
 /**
  * Check if Page component has a layout property; and if yes, wrap the page
  * into the specified layout, then render to a string.
  */
 function renderToString(page) {
-  var layout = null, child = null, props = {};
-  while ((layout = page.type.layout || (page.defaultProps && page.defaultProps.layout))) {
-    child = React.createElement(page, props, child);
-    _.extend(props, page.defaultProps);
-    React.renderToString(React.createElement(page, props, child));
-    page = layout;
-  }
-  return React.renderToString(React.createElement(page, props, child));
+    var layout = null, child = null, props = {};
+    while ((layout = page.layout || (page.defaultProps && page.defaultProps.layout))) {
+        child = React.createElement(page, props, child);
+        extend(props, page.defaultProps);
+        React.renderToString(React.createElement(page, props, child));
+        page = layout;
+    }
+    return React.renderToString(React.createElement(page, props, child));
 }
 
 /**
@@ -51,100 +45,101 @@ function renderToString(page) {
  * http://facebook.github.io/react/docs/top-level-api.html#react.rendertostaticmarkup
  */
 function renderToStaticMarkup(page) {
-  return React.renderToStaticMarkup(React.createElement(page));
+    return React.renderToStaticMarkup(React.createElement(page));
 }
 
 // Plugin level function (dealing with files)
 function Plugin(options) {
+    var babelOptions,
+        originalJsTransform;
 
-  options = options || {};
+    options = options || {};
 
-  var reactOptions = {
-    harmony: typeof options.harmony === 'undefined' ? true : options.harmony,
-    stripTypes: typeof options.stripTypes === 'undefined' ? true : options.stripTypes
-  };
+    babelOptions = options.babelOptions || {};
 
-  if (options.template && options.template.indexOf('<') === -1) {
-    options.template = fs.readFileSync(options.template, {encoding: 'utf8'});
-  }
-
-  var originalJsTransform = require.extensions['.js'];
-
-  var reactTransform = function(module, filename) {
-    if (filename.indexOf('node_modules') === -1) {
-      var src = fs.readFileSync(filename, {encoding: 'utf8'});
-      src = appendJsxPragma(filename, src);
-      src = ReactTools.transform(src, reactOptions);
-      module._compile(src, filename);
-    } else {
-      originalJsTransform(module, filename);
+    if (options.template && options.template.indexOf('<') === -1) {
+        options.template = fs.readFileSync(options.template, {encoding: 'utf8'});
     }
-  };
 
-  require.extensions['.js'] = reactTransform;
-  require.extensions['.jsx'] = reactTransform;
+    originalJsTransform = require.extensions['.js'];
 
-  // Creates a stream through which each file will pass
-  var stream = through.obj(function(file, enc, cb) {
-
-    if (!file.isNull()) {
-
-      if (file.isStream()) {
-        this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
-        return cb();
-      }
-
-      if (file.isBuffer()) {
-
-        try {
-          var contents = file.contents.toString('utf8');
-          contents = appendJsxPragma(file.path, contents);
-          contents = ReactTools.transform(contents, reactOptions);
-          var m = new Module();
-          m.id = file.path;
-          m.filename = file.path;
-          m.paths = module.paths.slice(1);
-          m._compile(contents, file.path);
-          var Component = m.exports;
-          var markup = options.staticMarkup ? renderToStaticMarkup(Component) : renderToString(Component);
-
-          if (options.template) {
-            var data = _.extend({}, (typeof(options.data) == 'function' ? options.data(file) : options.data));
-            data.body = markup;
-
-            // Set default values to avoid null-reference exceptions
-            data.title = data.title || '';
-            data.description = data.description || '';
-            data.keywords = data.keywords || '';
-
-            markup = template(options.template, data);
-          }
-
-          file.contents = new Buffer(markup);
-          var filename = gutil.replaceExtension(file.path, '.html');
-
-          if (typeof options.hyphenate === 'undefined' || options.hyphenate) {
-            filename = hyphenate(path.basename(filename));
-            filename = filename.lastIndexOf('-', 0) === 0 ? filename.substring(1) : filename;
-            filename = path.join(path.dirname(file.path), filename);
-          }
-
-          file.path = filename;
-        } catch (err) {
-          this.emit('error', new PluginError(PLUGIN_NAME, err));
-          return cb();
+    var reactTransform = function(module, filename) {
+        var src;
+        if (filename.indexOf('node_modules') === -1) {
+            src = fs.readFileSync(filename, {encoding: 'utf8'});
+            src = babel.transform(src, babelOptions);
+            module._compile(src, filename);
+        } else {
+            originalJsTransform(module, filename);
         }
-      }
-    }
+    };
 
-    // Make sure the file goes through the next gulp plugin
-    this.push(file);
-    // Tell the stream engine that we are done with this file
-    return cb();
-  });
+    require.extensions['.js'] = reactTransform;
+    require.extensions['.jsx'] = reactTransform;
 
-  // Return the file stream
-  return stream;
+    // Creates a stream through which each file will pass
+    var stream = through.obj(function(file, enc, cb) {
+        var contents, m, Component, markup, compiled, data, filename;
+        if (!file.isNull()) {
+
+            if (file.isStream()) {
+                this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+                return cb();
+            }
+
+            if (file.isBuffer()) {
+
+                try {
+                    contents = file.contents.toString('utf8');
+                    contents = babel.transform(contents, babelOptions);
+
+                    m = new Module();
+                    m.id = file.path;
+                    m.filename = file.path;
+                    m.paths = module.paths.slice(1);
+                    m._compile(contents.code, file.path);
+                    Component = m.exports;
+
+                    markup = options.staticMarkup ? renderToStaticMarkup(Component) : renderToString(Component);
+
+                    if (options.template) {
+                        compiled = template(options.template);
+                        data = extend({}, (typeof options.data === 'function' ? options.data(file) : options.data));
+                        data.body = markup;
+
+                        // Set default values to avoid null-reference exceptions
+                        data.title = data.title || '';
+                        data.description = data.description || '';
+                        data.keywords = data.keywords || '';
+
+                        markup = compiled(data);
+                    }
+
+                    file.contents = new Buffer(markup);
+                    filename = gutil.replaceExtension(file.path, '.html');
+
+                    if (typeof options.hyphenate === 'undefined' || options.hyphenate) {
+                        filename = hyphenate(path.basename(filename));
+                        filename = filename.lastIndexOf('-', 0) === 0 ? filename.substring(1) : filename;
+                        filename = path.join(path.dirname(file.path), filename);
+                    }
+
+                    file.path = filename;
+                } catch (err) {
+                    this.emit('error', new PluginError(PLUGIN_NAME, err));
+                    return cb();
+                }
+            }
+        }
+
+        // Make sure the file goes through the next gulp plugin
+        this.push(file);
+        // Tell the stream engine that we are done with this file
+        return cb();
+    });
+
+    // Return the file stream
+    return stream;
 }
 
 module.exports = Plugin;
--- test.js
+++ test.js
@@ -1,12 +1,10 @@
 /*
- * gulp-render
- * https://github.com/koistya/gulp-render
- *
- * Copyright (c) 2014 Konstantin Tarkus
- * Licensed under the MIT license
- */
-
-/* global require, Buffer, it */
+* gulp-render-react
+* https://github.com/koistya/gulp-render
+*
+* Copyright (c) 2014 Konstantin Tarkus
+* Licensed under the MIT license
+*/
 
 'use strict';
 
@@ -16,174 +14,174 @@ var render = require('./');
 
 it('Should render a simple React component', function(cb) {
 
-  var stream = render();
+    var stream = render();
 
-  stream.on('data', function(file) {
-    var contents = file.contents.toString('utf8');
-    assert(contents.indexOf('Hello world!') != -1);
-    cb();
-  });
+    stream.on('data', function(file) {
+        var contents = file.contents.toString('utf8');
+        assert(contents.indexOf('Hello world!') !== -1);
+        cb();
+    });
 
-  stream.write(new gutil.File({
-    path: 'SampleComponent.jsx',
-    cwd: __dirname,
-    contents: new Buffer(
-      'var React = require("./node_modules/react"); ' +
-      'var HelloMessage = React.createClass({' +
-      '  render: function() {return React.DOM.div(null, "Hello world!");}' +
-      '}); '+
-      'module.exports = HelloMessage;'
-    )
-  }));
+    stream.write(new gutil.File({
+        path: 'SampleComponent.jsx',
+        cwd: __dirname,
+        contents: new Buffer(
+            'var React = require("./node_modules/react"); ' +
+            'var HelloMessage = React.createClass({' +
+            '  render: function() {return React.DOM.div(null, "Hello world!");}' +
+            '}); ' +
+            'module.exports = HelloMessage;'
+        )
+    }));
 
 });
 
 it('Should render a simple React component with a template', function(cb) {
 
-  var stream = render({
-    template: '<html><head><title><%=title%></title></head><body><%=body%></body></html>',
-    data: { title: 'Title123' }
-  });
+    var stream = render({
+        template: '<html><head><title><%=title%></title></head><body><%=body%></body></html>',
+        data: { title: 'Title123' }
+    });
 
-  stream.on('data', function(file) {
-    var contents = file.contents.toString('utf8');
-    assert(contents.indexOf('Hello world!') != -1);
-    assert(contents.indexOf('<title>Title123</title>') != -1);
-    cb();
-  });
+    stream.on('data', function(file) {
+        var contents = file.contents.toString('utf8');
+        assert(contents.indexOf('Hello world!') !== -1);
+        assert(contents.indexOf('<title>Title123</title>') !== -1);
+        cb();
+    });
 
-  stream.write(new gutil.File({
-    path: 'SampleComponent.jsx',
-    cwd: __dirname,
-    contents: new Buffer(
-      'var React = require("./node_modules/react"); ' +
-      'var HelloMessage = React.createClass({' +
-      '  render: function() {return React.DOM.div(null, "Hello world!");}' +
-      '}); '+
-      'module.exports = HelloMessage;'
-    )
-  }));
+    stream.write(new gutil.File({
+        path: 'SampleComponent.jsx',
+        cwd: __dirname,
+        contents: new Buffer(
+            'var React = require("./node_modules/react"); ' +
+            'var HelloMessage = React.createClass({' +
+            '  render: function() {return React.DOM.div(null, "Hello world!");}' +
+            '}); ' +
+            'module.exports = HelloMessage;'
+        )
+    }));
 
 });
 
 it('Should render a simple React component as static markup', function(cb) {
 
-  var stream = render({
-    staticMarkup: true
-  });
+    var stream = render({
+        staticMarkup: true
+    });
 
-  stream.on('data', function(file) {
-    var contents = file.contents.toString('utf8');
-    assert(contents.indexOf('data-react') === -1);
-    cb();
-  });
+    stream.on('data', function(file) {
+        var contents = file.contents.toString('utf8');
+        assert(contents.indexOf('data-react') === -1);
+        cb();
+    });
 
-  stream.write(new gutil.File({
-    path: 'SampleComponent.jsx',
-    cwd: __dirname,
-    contents: new Buffer(
-      'var React = require("./node_modules/react"); ' +
-      'var HelloMessage = React.createClass({' +
-      '  render: function() {return React.DOM.div(null, "Hello world!");}' +
-      '}); '+
-      'module.exports = HelloMessage;'
-    )
-  }));
+    stream.write(new gutil.File({
+        path: 'SampleComponent.jsx',
+        cwd: __dirname,
+        contents: new Buffer(
+            'var React = require("./node_modules/react"); ' +
+            'var HelloMessage = React.createClass({' +
+            '  render: function() {return React.DOM.div(null, "Hello world!");}' +
+            '}); ' +
+            'module.exports = HelloMessage;'
+        )
+    }));
 
 });
 
 it('Should render a simple React component with a template and data function', function(cb) {
 
-  var stream = render({
-    template: '<html><head><title><%=title%></title></head><body><%=body%></body></html>',
-    data: function(file) {
-      return { title: 'Test123' + file.path };
-    }
-  });
+    var stream = render({
+        template: '<html><head><title><%=title%></title></head><body><%=body%></body></html>',
+        data: function(file) {
+            return { title: 'Test123' + file.path };
+        }
+    });
 
-  stream.on('data', function(file) {
-    var contents = file.contents.toString('utf8');
-    assert(contents.indexOf('Hello world!') != -1);
-    assert(contents.indexOf('<title>Test123SampleComponent.jsx</title>') != -1);
-    cb();
-  });
+    stream.on('data', function(file) {
+        var contents = file.contents.toString('utf8');
+        assert(contents.indexOf('Hello world!') !== -1);
+        assert(contents.indexOf('<title>Test123SampleComponent.jsx</title>') !== -1);
+        cb();
+    });
 
-  stream.write(new gutil.File({
-    path: 'SampleComponent.jsx',
-    cwd: __dirname,
-    contents: new Buffer(
-      'var React = require("./node_modules/react"); ' +
-      'var HelloMessage = React.createClass({' +
-      '  render: function() {return React.DOM.div(null, "Hello world!");}' +
-      '}); '+
-      'module.exports = HelloMessage;'
-    )
-  }));
+    stream.write(new gutil.File({
+        path: 'SampleComponent.jsx',
+        cwd: __dirname,
+        contents: new Buffer(
+            'var React = require("./node_modules/react"); ' +
+            'var HelloMessage = React.createClass({' +
+            '  render: function() {return React.DOM.div(null, "Hello world!");}' +
+            '}); ' +
+            'module.exports = HelloMessage;'
+        )
+    }));
 
 });
 
 it('Should render a React component with a layout defined in default props', function(cb) {
 
-  var stream = render();
+    var stream = render();
 
-  stream.on('data', function(file) {
-    var contents = file.contents.toString('utf8');
-    assert(contents.indexOf('Layout') != -1);
-    assert(contents.indexOf('Test') != -1);
-    assert(contents.indexOf('Hello world!') != -1);
-    cb();
-  });
+    stream.on('data', function(file) {
+        var contents = file.contents.toString('utf8');
+        assert(contents.indexOf('Layout') !== -1);
+        assert(contents.indexOf('Test') !== -1);
+        assert(contents.indexOf('Hello world!') !== -1);
+        cb();
+    });
 
-  stream.write(new gutil.File({
-    path: 'SampleComponent.jsx',
-    cwd: __dirname,
-    contents: new Buffer(
-      'var React = require("./node_modules/react"); ' +
-      'var Layout = React.createClass({' +
-      '  render: function() {return React.DOM.div(null, ["Layout", this.props.title, this.props.children]);}' +
-      '}); '+
-      'var HelloMessage = React.createClass({' +
-      '  getDefaultProps() {' +
-      '    return {' +
-      '      "title": "Test",' +
-      '      "layout": Layout' +
-      '    }' +
-      '  },' +
-      '  render: function() {return React.DOM.div(null, "Hello world!");}' +
-      '}); '+
-      'module.exports = HelloMessage;'
-    )
-  }));
+    stream.write(new gutil.File({
+        path: 'SampleComponent.jsx',
+        cwd: __dirname,
+        contents: new Buffer(
+            'var React = require("./node_modules/react"); ' +
+            'var Layout = React.createClass({' +
+            '  render: function() {return React.DOM.div(null, ["Layout", this.props.title, this.props.children]);}' +
+            '}); ' +
+            'var HelloMessage = React.createClass({' +
+            '  getDefaultProps() {' +
+            '    return {' +
+            '      "title": "Test",' +
+            '      "layout": Layout' +
+            '    }' +
+            '  },' +
+            '  render: function() {return React.DOM.div(null, "Hello world!");}' +
+            '}); ' +
+            'module.exports = HelloMessage;'
+        )
+    }));
 
 });
 
 it('Should render a React component with a layout defined in statics', function(cb) {
 
-  var stream = render();
+    var stream = render();
 
-  stream.on('data', function(file) {
-    var contents = file.contents.toString('utf8');
-    assert(contents.indexOf('Layout') != -1);
-    assert(contents.indexOf('Hello world!') != -1);
-    cb();
-  });
+    stream.on('data', function(file) {
+        var contents = file.contents.toString('utf8');
+        assert(contents.indexOf('Layout') !== -1);
+        assert(contents.indexOf('Hello world!') !== -1);
+        cb();
+    });
 
-  stream.write(new gutil.File({
-    path: 'SampleComponent.jsx',
-    cwd: __dirname,
-    contents: new Buffer(
-      'var React = require("./node_modules/react"); ' +
-      'var Layout = React.createClass({' +
-      '  render: function() {return React.DOM.div(null, ["Layout", this.props.children]);}' +
-      '}); '+
-      'var HelloMessage = React.createClass({' +
-      '  statics: {' +
-      '    "layout": Layout' +
-      '  },' +
-      '  render: function() {return React.DOM.div(null, "Hello world!");}' +
-      '}); '+
-      'module.exports = HelloMessage;'
-    )
-  }));
+    stream.write(new gutil.File({
+        path: 'SampleComponent.jsx',
+        cwd: __dirname,
+        contents: new Buffer(
+            'var React = require("./node_modules/react"); ' +
+            'var Layout = React.createClass({' +
+            '  render: function() {return React.DOM.div(null, ["Layout", this.props.children]);}' +
+            '}); ' +
+            'var HelloMessage = React.createClass({' +
+            '  statics: {' +
+            '    "layout": Layout' +
+            '  },' +
+            '  render: function() {return React.DOM.div(null, "Hello world!");}' +
+            '}); ' +
+            'module.exports = HelloMessage;'
+        )
+    }));
 
 });
